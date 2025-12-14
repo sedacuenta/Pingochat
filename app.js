40fdfe03dc649cccb478a4bcb1bbd065ed4f7641
sedacuenta <sedacuenta7777@gmail.com>
2025-12-14 22:19:18 +0100

    Actualizar

---
+++ app.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyChee8wpWx9i2Az9U6aeYscRo14E7JuEWo",
+  authDomain: "pingochat-d18b3.firebaseapp.com",
+  projectId: "pingochat-d18b3",
+  storageBucket: "pingochat-d18b3.firebasestorage.app",
+  messagingSenderId: "27442296634",
+  appId: "1:27442296634:web:c372e481e498eb66980203",
+  measurementId: "G-9CR2H5R418"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
